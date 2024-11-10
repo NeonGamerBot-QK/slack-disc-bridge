6d1b0d66a326d85d395b240cd9ef56f2e716b8ac
Saahil <neon@saahild.com>
2024-11-09 23:24:46 -0500

    update: add express somewhat stable

---
+++ src/modules/express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const expressApp = express();
+
+expressApp.use(express.json());
+expressApp.use(express.urlencoded({ extended: true }));
+expressApp.use(express.static("src/public"));
+expressApp.set("view engine", "ejs");
+expressApp.set("views", "src/views");
+expressApp.get("/", (req, res) => {
+  res.render("index")
+});
+module.exports = expressApp;
--- src/modules/slack.js
+++ src/modules/slack.js
@@ -1,20 +1,20 @@
 const { App } = require("@slack/bolt");
-const expressApp = require("express")();
-expressApp.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const expressApp = require("./express");
+console.log(expressApp._router.stack, expressApp._router.stack.filter(r=> r.name == "bound dispatch").map((r) => ({
+  handler: expressApp,
+   route: r.route.path,
+   method: r.route.methods,
+ })))
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   port: process.env.PORT,
   ignoreSelf: true,
   //   developerMode: true,
-  customRoutes: [
-    {
-      path: "/",
-      method: "GET",
-      handler: expressApp,
-    },
-  ],
+  customRoutes: expressApp._router.stack.filter(r=> r.name == "bound dispatch").map((r) => ({
+   handler: expressApp,
+    path: r.route.path,
+    method: Object.keys(r.route.methods).map(e=>e.toUpperCase()),
+  })),
 });
 module.exports = app;
